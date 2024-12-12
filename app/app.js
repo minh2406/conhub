ea5a9a12a1eb47e8d45f70523fd4d17d447540a4
minh2406 <70198366+minh2406@users.noreply.github.com>
2024-12-12 15:49:58 +0700

    fix form

--- app/app.js
+++ app/app.js
@@ -1,23 +1,5 @@
 //app.js
-/*
-const http = require('http')
-const port = 8080
-//asdasdsa
-var fs = require('fs');
-//express
-app.use(express.static(__dirname + '/public'));
 
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(port);
-});
-*/
 var express = require('express');
 var app = express();
 var port = 8080;
@@ -30,10 +12,10 @@ require('dotenv').config();
 //postgreSQL
 const User = process.env.PG_User;
 const Password = process.env.PG_Password;
-const Database =  process.env.PG_Database;
+const Database = process.env.PG_Database;
 const Host = process.env.PG_Host;
 const Port = process.env.PG_Port;
-const {Client} = require('pg');
+const { Client } = require('pg');
 const client = new Client({
   host: Host,
   user: User,
@@ -44,82 +26,75 @@ const client = new Client({
 })
 
 client.connect();
-client.query('Select * from films', (err, res)=>{
-    if(!err){
+function fetchAndRender(require, result, web, title)
+{
+  client.query('Select * from films', (err, res) => {
+    if (!err) {
       let fetchData = res.rows;
-      app.get('/', function(req, res){
-        res.render('index.ejs', {
-              title: 'Trang chủ - Con hub',
-              dbData: fetchData,
-        });
+      result.render(web, {
+        title: title,
+        url: require.originalUrl,
+        dbData: fetchData,
       });
-      console.log(fetchData);
-      app.get('/home', function(req, res){
-        res.render('index.ejs', {
-              title: 'Trang chủ - Con hub',
-              dbData: fetchData,
-        });
-      });
-      app.get('/watch/*', function(req, res){
-        res.render('viewFilm.ejs', {
-          title: 'Xem phim - Con hub',
-          url: req.originalUrl,
-          dbData: fetchData,
-        });
-      });
-      app.get('/year/(*)', function(req, res){
-        res.render('yearFilter.ejs', {
-          title: 'Năm phát hành - Con hub',
-          url: req.originalUrl,
-          dbData: fetchData,
-        });
-      });
-      app.get('/form', function(req, res){
-        res.render('form.ejs', {
-          title: 'Đăng tải phim - Con hub',
-          url: req.originalUrl,
-          dbData: fetchData,
-        });
-      });
-      app.listen(port);
-      console.log("web is running");
     }
     else {
       console.log(err.message);
     }
+  });
+}
+app.get('/', function (req, res) {
+  fetchAndRender(req, res, 'index.ejs', 'Trang chủ - Con hub');
 });
+app.get('/home', function (req, res) {
+  fetchAndRender(req, res, 'index.ejs', 'Trang chủ - Con hub');
+});
+app.get('/watch/*', function (req, res) {
+  fetchAndRender(req, res, 'viewFilm.ejs', 'Xem phim - Con hub');
+});
+app.get('/year/(*)', function (req, res) {
+  fetchAndRender(req, res, 'yearFilter.ejs', 'Năm phát hành - Con hub'); 
+});
+app.get('/form', function (req, res) {
+  fetchAndRender(req, res, 'form.ejs', 'Đăng tải phim - Con hub');
+});
+app.get('/submit', function (req, res) {
+  fetchAndRender(req, res, 'filmSubmit.ejs', 'Đăng tải phim - Con hub');
+});
+app.listen(port);
+console.log("web is running");
+
 //body-parser
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-app.post('/submit', function(req, res){
-  const {name, description, author, year, url, source} = req.body;
+app.post('/submit', function (req, res) {
+  const { name, description, author, year, url, source } = req.body;
   addData(filter(name), filter(description), filter(author), filter(year), filter(url), filter(source));
+  res.redirect('/submit');
 })
-function filter(data)
-{
+function filter(data) {
   var filted = data;
   filted = filted.replace(/"/g, '\\"');
   filted = filted.replace(/\r\n/g, '<br>');
   return filted;
 }
-function addData(name, description, author, year, url, source)
-{
-  console.log(name,"\n", `"${description}"`,"\n", author,"\n", year,"\n", url,"\n", source);
+function addData(name, description, author, year, url, source) {
+  console.log(name, "\n", `"${description}"`, "\n", author, "\n", year, "\n", url, "\n", source);
   client.query(`INSERT INTO films (name, description, author, year, img, src)
   VALUES ($$${name}$$,
       $$${description}$$,
       $$${author}$$,
       ${year},
       $$${url}$$,
-      $$${source}$$);`, (err, res)=>{
-    if(!err){
+      $$${source}$$);`, (err, res) => {
+    if (!err) {
       console.log(res.rows);
     }
     else {
       console.log(err.message);
     }
-    client.end;
+    //client.end;
+    console.log("a");
   });
 }
